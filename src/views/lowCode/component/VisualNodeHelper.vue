44779d9eef7363410231bfa7a17ef9330cbfd186
jiangzhe <jiangzhe.jz@quadtalent.com>
2022-03-28 11:30:53 +0800

    边框样式

--- src/views/lowCode/component/VisualNodeHelper.vue
+++ src/views/lowCode/component/VisualNodeHelper.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
-import { computed, defineProps, onMounted, watch, watchEffect } from 'vue'
-import { Num } from 'windicss/types/lang/tokens'
+import { defineProps } from 'vue'
 
 defineProps({
-  location:Object,
+  location: {
+    type: Object,
+    required: true
+  },
+  name: {
+    type: String,
+    default: '虚拟边框'
+  }
 })
 
 </script>
 
 <template>
-    <div
-        class="absolute border-dashed border-1 border-light-blue-500 bg-light-blue-100 bg-opacity-25"
-         :style="{top:location.top+'px',width:location.width+'px',height:location.height+'px',left:location.left+'px'}">
-    </div>
+  <div
+      class="absolute border-dashed border-1 border-light-blue-500 bg-light-blue-100 bg-opacity-25"
+      :style="{top:location.top+'px',width:location.width+'px',height:location.height+'px',left:location.left+'px'}">
+    <p class="absolute -top-20px text-blue-400 text-sm">{{ name }}</p>
+  </div>
 </template>
 
 <style>
