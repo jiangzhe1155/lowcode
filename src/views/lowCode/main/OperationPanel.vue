727c909d7b03c544e5f51bcfa170a2e65a670fdc
jiangzhe <jiangzhe.jz@quadtalent.com>
2022-04-07 18:53:01 +0800

    优化编辑器

--- src/views/lowCode/main/OperationPanel.vue
+++ src/views/lowCode/main/OperationPanel.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import { computed, h, onBeforeUpdate, onMounted, onUpdated, resolveComponent } from 'vue'
+import { computed, createElementBlock, h, onBeforeUpdate, onMounted, onUpdated, resolveComponent } from 'vue'
 import { useRenderPageData } from '@/views/lowCode/service'
 
 const { renderPage } = useRenderPageData('12312')
@@ -10,6 +10,7 @@ function doRender (node: any) {
   if (node.visible) {
     return h(resolve, {
       element: node,
+      id: node.id
     }, () => {
       return node.children.map((e: any) => doRender(e))
     })
@@ -18,20 +19,27 @@ function doRender (node: any) {
 
 onMounted(() => {
   console.log('初始化生成')
+  let elementById = document.getElementById(renderPage.value.components[0].id)
+  console.log(elementById)
 })
 
 onBeforeUpdate(() => {
   console.log('更新')
 })
+
 const componentRender = computed(() => {
   // 2.渲染对话框
-  return h('div', {}, [...renderPage.value.components, ...renderPage.value.models,].map(m => doRender(m)).filter(m => m))
+  return h('div', { id: 'asdad' }, [...renderPage.value.components, ...renderPage.value.models,].map(m => doRender(m)).filter(m => m))
 })
+
+function render () {
+}
 
 </script>
 
 <template>
   <componentRender/>
+  <render></render>
 </template>
 <style scoped>
 
