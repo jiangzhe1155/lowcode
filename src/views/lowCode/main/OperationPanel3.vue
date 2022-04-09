62a98b16a0df84e212a91cee8961dfb48b36ae4b
jiangzhe <jiangzhe.jz@quadtalent.com>
2022-04-09 20:50:11 +0800

    优化编辑器

---
+++ src/views/lowCode/main/OperationPanel3.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+import { computed, createElementBlock, h, onBeforeUpdate, onMounted, onUpdated, resolveComponent } from 'vue'
+import { useRenderPageData } from '@/views/lowCode/service'
+
+const {
+  renderPage,
+  locationMap
+} = useRenderPageData('12312')
+
+function doRender (node: any) {
+  const resolve = resolveComponent(node.type)
+  if (node.visible) {
+    return h(resolve, {
+      element: node,
+      id: node.id
+    }, () => {
+      return node.children.map((e: any) => doRender(e))
+    })
+  }
+}
+
+const componentRender = computed(() => {
+  // 2.渲染对话框
+  return h('div', { class: '!min-h-100vh flex !flex-col' }, [...renderPage.components, ...renderPage.models,].map(m => doRender(m)).filter(m => m))
+})
+
+</script>
+
+<template>
+  <componentRender></componentRender>
+</template>
+<style scoped>
+
+</style>
