193f55f6a09a22ab06146506f145e64ccc31ed2c
jiangzhe <1024741733@qq.com>
2022-04-27 23:44:39 +0800

    尝试数据源

--- src/components/table/Table.vue
+++ src/components/table/Table.vue
@@ -1,70 +1,101 @@
 <script lang="ts" setup>
-import { Table } from "@/views/designer/service/component";
-import { computed, onMounted, ref, watch } from "vue";
-import { getProp } from "@/views/designer/service/propsWatcher";
-import { Column } from "@/components/table/ColumnType";
-import Item from "@/views/designer/config/panel/Item.vue";
-import ItemLabel from "@/views/designer/config/panel/ItemLabel.vue";
-import Button from "@/components/button/Button.vue";
-import axios from "axios";
+import { Table } from '@/views/designer/service/component'
+import { computed, onMounted, ref, watch } from 'vue'
+import { getProp } from '@/views/designer/service/propsWatcher'
+import { Column } from '@/components/table/ColumnType'
+import Item from '@/views/designer/config/panel/Item.vue'
+import ItemLabel from '@/views/designer/config/panel/ItemLabel.vue'
+import Button from '@/components/button/Button.vue'
+import axios from 'axios'
 
 const props = defineProps<{
   component: Table,
   state: any
-}>();
+}>()
 
 const rowData = computed(() => {
-  let res = getProp(props.component.props.rowData);
-  let idx = props.component.props.rowData.idx;
-  if (idx === "array") {
-    return res;
-  } else if (idx === "variable") {
+  let res = getProp(props.component.props.rowData)
+  let idx = props.component.props.rowData.idx
+  if (idx === 'array') {
+    return res
+  } else if (idx === 'variable') {
     try {
-      return eval(`props.state.value.${res}`);
+      return eval(`props.state.value.${res}`)
     } catch (e) {
     }
   }
-});
+})
 
 const columns = computed((): Column[] => {
-  return getProp(props.component.props.columns);
-});
+  return getProp(props.component.props.columns)
+})
 
 const toolbars = computed((): any[] => {
-  return getProp(props.component.props.toolbars);
-});
+  return getProp(props.component.props.toolbars)
+})
 
 const operations = computed((): any[] => {
-  return getProp(props.component.props.operations);
-});
+  return getProp(props.component.props.operations)
+})
 
-const searchFilter = ref({});
+const searchFilter = ref({})
 const searchColumn = computed(() => {
-  return columns.value.filter(c => c.search);
-});
+  return columns.value.filter(c => c.search)
+})
 
-const filterRowCount = ref(6);
+const filterRowCount = ref(6)
 const gridStyle = computed(() => {
   if (filterRowCount.value == 3) {
-    return "grid-cols-3";
+    return 'grid-cols-3'
   }
   if (filterRowCount.value == 4) {
-    return "grid-cols-4";
+    return 'grid-cols-4'
   }
   if (filterRowCount.value == 5) {
-    return "grid-cols-5";
+    return 'grid-cols-5'
   }
   if (filterRowCount.value == 6) {
-    return "grid-cols-6";
+    return 'grid-cols-6'
   }
-});
+})
+
+const pageNo = ref(1)
+const pageSize = ref(10)
+const total = ref(0)
 
 onMounted(() => {
-  axios.get(`/api/getUsers`, {}).then(res => {
-    console.log("获取到数据", res.data);
-    props.state.value.userList = res.data.data;
-  });
-});
+  axios.post(`/api/getUsers`, {
+    pageNo: pageNo.value,
+    pageSize: pageSize.value
+  }).then(res => {
+    console.log('获取到数据', res.data)
+    props.state.value.userList = res.data.data
+    pageNo.value = res.data.pageNo
+    pageSize.value = res.data.pageSize
+    total.value = res.data.total
+  })
+})
+
+const handleSizeChange = () => {
+  axios.post(`/api/getUsers`, {
+    pageNo: pageNo.value,
+    pageSize: pageSize.value
+  }).then(res => {
+    props.state.value.userList = res.data.data
+    total.value = res.data.total
+  })
+}
+
+const handleCurrentChange = () => {
+  axios.post(`/api/getUsers`, {
+    pageNo: pageNo.value,
+    pageSize: pageSize.value
+  }).then(res => {
+    console.log('获取到数据', res.data)
+    props.state.value.userList = res.data.data
+    total.value = res.data.total
+  })
+}
 
 </script>
 
@@ -94,9 +125,9 @@ onMounted(() => {
 
     <el-table :data="rowData" style="width: 100%">
       <el-table-column
-        v-for="(column,idx) in columns.filter(c=>!c.hidden)" :key="idx"
-        :label="column.title"
-        :prop="column.key" />
+          v-for="(column,idx) in columns.filter(c=>!c.hidden)" :key="idx"
+          :label="column.title"
+          :prop="column.key"/>
 
       <el-table-column label="操作" width="120">
         <template #default="{row}">
@@ -108,10 +139,16 @@ onMounted(() => {
         </template>
       </el-table-column>
     </el-table>
-
-    <el-pagination :total="1000" background
+    {{ pageSize }} {{ pageNo }}
+    <el-pagination v-model:currentPage="pageNo"
+                   v-model:page-size="pageSize"
+                   :page-sizes="[10, 50, 100]"
+                   :total="total"
+                   background
                    class="mt-20px"
-                   layout="total,sizes, prev, pager, next" />
+                   layout="total,sizes, prev, pager, next"
+                   @size-change="handleSizeChange"
+                   @current-change="handleCurrentChange"/>
   </div>
 
 </template>
--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,12 +1,27 @@
 export default [
   {
-    url: "/api/getUsers",
-    method: "get",
+    url: '/api/getUsers',
+    method: 'post',
     response: () => {
       return {
         code: 0,
-        message: "ok",
-        data: [{name:'张三',sex:'男'},{name:'李四',sex:'女'},{name:'张三',sex:'男'},{name:'李四',sex:'女'}],
+        message: 'ok',
+        data: [{
+          name: '张三',
+          sex: '男'
+        }, {
+          name: '李四',
+          sex: '女'
+        }, {
+          name: '张三',
+          sex: '男'
+        }, {
+          name: '李四',
+          sex: '女'
+        }],
+        total: 20,
+        pageNo: 1,
+        pageSize: 10
       }
     }
   }
